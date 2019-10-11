46c08b2cc9aee5f31f49778ddfdad45ec393fc0d
Stephanie Fillmon <stephanie.fillmon@rackspace.com>
2019-10-11 13:52:09 -0500

    adjustments for pdf

--- docs/conf.py
+++ docs/conf.py
@@ -37,17 +37,19 @@ except:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# ,
+# 'chios.bolditalic',
+# 'chios.remotecode',
+# 'chios.remoteinclude'
 extensions = [
     'sphinx.ext.githubpages',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.todo',
-    'chios.bolditalic',
-    'chios.remotecode',
-    'chios.remoteinclude'
+    'sphinx.ext.todo'
 ]
 
 if spelling is not None:
     extensions.append('sphinxcontrib.spelling')
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
